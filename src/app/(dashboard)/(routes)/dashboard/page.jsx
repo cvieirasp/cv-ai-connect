5e45ae657060d03f6c1a61f4d1a30c1b77277b4f
Carlos Figueiredo <cvieirasp@gmail.com>
2024-01-31 20:10:47 -0300

    criação da página de dashboard

--- src/app/(dashboard)/(routes)/dashboard/page.jsx
+++ src/app/(dashboard)/(routes)/dashboard/page.jsx
@@ -1,7 +1,86 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import {
+  ArrowRight,
+  MessageSquare,
+  Code,
+  ImageIcon,
+  Music,
+  VideoIcon,
+} from 'lucide-react'
+import { Card } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+
+const tools = [
+  {
+    label: 'Conversação',
+    icon: MessageSquare,
+    color: 'text-violet-500',
+    bgColor: 'bg-violet-500/10',
+    href: '/conversation',
+  },
+  {
+    label: 'Geração de Imagem',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    bgColor: 'bg-pink-700/10',
+    href: '/image',
+  },
+  {
+    label: 'Geração de Vídeo',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    bgColor: 'bg-orange-700/10',
+    href: '/video',
+  },
+  {
+    label: 'Geração de Áudio',
+    icon: Music,
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+    href: '/audio',
+  },
+  {
+    label: 'Geração de Código',
+    icon: Code,
+    color: 'text-green-700',
+    bgColor: 'bg-green-700/10',
+    href: '/code',
+  },
+]
+
 const DashboardPage = () => {
+  const router = useRouter()
+
   return (
     <div>
-      <h1>Dashboard Page (Protected)</h1>
+      <div className='mb-8 space-y-4'>
+        <h2 className='text-center text-2xl font-bold md:text-4xl'>
+          Explore o poder da Inteligência Artificial
+        </h2>
+        <p className='text-center text-sm font-light text-muted-foreground md:text-lg'>
+          Converse com a Inteligência Artificial mais inteligente e experimente
+          seu poder
+        </p>
+      </div>
+      <div className='space-y-4 px-4 md:px-20 lg:px-32'>
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className='flex cursor-pointer items-center justify-between border-black/5 p-4 transition hover:shadow-md'
+          >
+            <div className='flex items-center gap-x-4'>
+              <div className={cn('w-fit rounded-md p-2', tool.bgColor)}>
+                <tool.icon className={cn('h-8 w-8', tool.color)} />
+              </div>
+              <div className='font-semibold'>{tool.label}</div>
+            </div>
+            <ArrowRight className='h-5 w-5' />
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,7 +1,8 @@
-'user client'
+'use client'
 
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { Montserrat } from 'next/font/google'
 import {
   Code,
@@ -64,6 +65,8 @@ const routes = [
 ]
 
 const Sidebar = () => {
+  const pathname = usePathname()
+
   return (
     <div className='flex h-full flex-col space-y-4 bg-[#111827] py-4 text-white'>
       <div className='flex-1 px-3 py-2'>
@@ -80,7 +83,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className='group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white'
+              className={cn(
+                'group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white',
+                pathname === route.href
+                  ? 'bg-white/10 text-white'
+                  : 'text-zinc-400'
+              )}
             >
               <div className='flex flex-1 items-center'>
                 <route.icon className={cn('mr-3 h-5 w-5', route.color)} />
